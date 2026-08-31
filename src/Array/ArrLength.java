7d06dfdc6e9870d5331e41741164cb247e7ee7a4
Deepak Yadav <dyadav17322@gmail.com>
2026-08-31 23:54:24 +0530

    Added Java prectice programs

---
+++ src/Array/ArrLength.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class ArrLength {
+    static void main() {
+        int[] arr= {21,33,-9,83,74};
+        for (int i=0; i<arr.length;i++)
+        System.out.println(arr[i]);
+    }
+}
