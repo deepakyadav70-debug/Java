c4708cc168229e167c1ba4ecc2336841e17c1838
Deepak Yadav <dyadav17322@gmail.com>
2026-09-04 12:16:08 +0530

    Added Java prectice programs

--- src/Array/ShallowANDDeepCopy.java
+++ src/Array/ShallowANDDeepCopy.java
@@ -1,10 +1,13 @@
 package Array;
-
+import java.util.Arrays;
 public class ShallowANDDeepCopy {
     static void main() {
         int[] arr={10,20,30,40};
-        int[] x= arr;     // Here x is a Shallow copy of arr.
-        x[0]=100;
-        System.out.println(arr[0]);
+//        int[] x= arr;     // Here x is a Shallow copy of arr.
+//        x[0]=100;
+//        System.out.println(arr[0]);
+        int[]y = Arrays.copyOf(arr, arr.length); // deep copy
+        y[0]= 20;
+        System.out.print(arr[0]);
     }
 }
