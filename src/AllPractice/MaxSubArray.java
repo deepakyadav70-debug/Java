2107c4d36ae0f0fa2420532d8e330d62c4e83848
Deepak Yadav <dyadav17322@gmail.com>
2026-09-05 12:00:00 +0530

    Add Kadane's Algorithm for Maximum Subarray Sum

---
+++ src/AllPractice/MaxSubArray.java
@@ -0,0 +1,17 @@
+package AllPractice;
+
+public class MaxSubArray {
+    static void main() {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+        int maxSoFar = arr[0];
+        int currentMax = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentMax = Math.max(arr[i], currentMax + arr[i]);
+            maxSoFar = Math.max(maxSoFar, currentMax);
+        }
+
+        System.out.println("Maximum subarray sum: " + maxSoFar);
+    }
+}
