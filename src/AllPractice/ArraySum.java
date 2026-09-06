0a56f0ced5563c92038d65ba5ac06691fdb1bfe8
Deepak Yadav <dyadav17322@gmail.com>
2026-09-06 23:28:22 +0530

    Added Java prectice programs

---
+++ src/AllPractice/ArraySum.java
@@ -0,0 +1,14 @@
+package AllPractice;
+
+public class ArraySum {
+    static void main() {
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        System.out.println("Sum of array elements: " + sum);
+    }
+}
