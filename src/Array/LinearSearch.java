cab593d51906b348fe8b5ee0b72b2a97ce8dc018
Deepak Yadav <dyadav17322@gmail.com>
2026-09-07 23:48:36 +0530

    Added Java prectice programs

---
+++ src/Array/LinearSearch.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {12, 45, 7, 23, 56, 89, 34};
+        int target = 18;
+
+        int result = linearSearch(numbers, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in array.");
+        }
+    }
+}
