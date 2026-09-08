bbb19e7456ad1257df640863af53536c84ded536
Deepak Yadav <dyadav17322@gmail.com>
2026-09-08 23:46:37 +0530

    Added Java prectice programs

---
+++ src/Array/ForEachLoop.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class ForEachLoop {
+    static void main() {
+        int [] arr={2,4,6,8,10};
+        for( int ele: arr)
+            System.out.print(ele +" ");
+    }
+}
