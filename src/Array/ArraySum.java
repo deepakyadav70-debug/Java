124c9bb9cbf3dbe73bd7c24ed58d6fbe4e4aab24
Deepak Yadav <dyadav17322@gmail.com>
2026-09-08 19:50:37 +0530

    Added Java prectice programs

--- src/Array/ArraySum.java
+++ src/Array/ArraySum.java
@@ -1,12 +1,28 @@
 package Array;
 
+import java.util.Scanner;
+
 public class ArraySum {
     static void main() {
-        int [] arr={-2, 3, 4, 12,-9};
-        int sum=0;
-        for(int i=0; i<arr.length; i++){
-            sum += arr[i];
+//        int [] arr={-2, 3, 4, 12,-9};
+//        int sum=0;
+//        for(int i=0; i<arr.length; i++){
+//            sum += arr[i];
+//        }
+//        System.out.print(sum);
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter number of element in array: ");
+        int n=sc.nextInt();
+        int[] arr= new int[n];
+        System.out.print("Enter "+n+" integers");
+        for (int i=0 ;i<arr.length;i++) {
+            arr[i] = sc.nextInt();
         }
-        System.out.print(sum);
+        int sum=0;
+        for (int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        System.out.print("Sum= "+ sum);
     }
 }
